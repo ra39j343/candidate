56496c5e1214fd0b0eb9fe13614a8cdcad6c5c14
Radik <verdyanradik@gmail.com>
2025-01-29 16:53:12 +0300

    Update next-auth type declarations

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,17 +4,21 @@ import { JWT } from 'next-auth/jwt'
 declare module 'next-auth' {
   interface Session {
     user: {
-      id: string
+      id: string;
+      role?: string;
     } & DefaultSession['user']
   }
 
   interface User extends DefaultUser {
-    id: string
+    id: string;
+    role?: string;
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    id?: string
+    id?: string;
+    role?: string;
+    username?: string;
   }
 } 
